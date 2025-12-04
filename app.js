9a5d96cbaa61246336128286bec1837116c4c728
baocuong <lybaocuong@gmail.com>
2025-12-04 22:25:10 +0700

    commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { startServer } from "./src/api.js";
 import { startScheduler } from "./src/scheduler.js";
 
-const today = new Date().toISOString();
+const today = new Date().toISOString().split("T")[0];
 console.log("Server started on", today);
 
 startServer();
